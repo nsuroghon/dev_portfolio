6f972c2096f6a4769ef591e2c2a79d3a5df34e1d
niv suroghon <nivsuroghon@nivs-MacBook-Pro.local>
2022-03-08 11:52:04 -0500

    starter routs and sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import './styles/main.scss';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+// Pages
+import About from './pages/About';
+import Landing from './pages/Landing';
+import Projects from './pages/Projects';
+
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Landing />}/>
+        {/* <div className="App">
+        hello world
+        </div> */}
+        <Route path='/projects' element={<Projects />}/>
+
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 

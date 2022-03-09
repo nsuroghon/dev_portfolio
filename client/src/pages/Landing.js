229196ce29131384f4d83ec421eb4b6d34830467
niv suroghon <nivsuroghon@nivs-MacBook-Pro.local>
2022-03-09 15:45:48 -0500

    styling for landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className='landing'>
+        <div className='landing__logo-box'>
+          <img className='landing__logo' alt='logo'></img>
+        </div>
+        <div className='landing__nav-box'>
+          <a className='btn btn--light'>Button</a>
+          <a className='btn btn--light'>Button</a>
+          <a className='btn btn--light'>Button</a>
+        </div>
+        <div className='landing__heading-box'>
+          <h1 className='heading-primary'></h1>
+        </div>
+        <div className="landing__email-box">
+          <a href='#' className='btn btn--dark'>Email</a>
+        </div>
+        <div className='socialsBox'>
+          <a href='#'>Social Media</a>
+        </div>
+    </div>
   )
 }
 

294d98f9a507238ff19b146b6b9a817594385c68
niv suroghon <nivsuroghon@nivs-MacBook-Pro.local>
2022-03-10 09:09:59 -0500

    contact page icon and text

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { MdOutlineMarkEmailRead } from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <div className='contact__icon-box'>
+        <MdOutlineMarkEmailRead size={200}/>
+      </div>
+      <div className="contact__inquiries">
+          <h1>
+            <h1>Please send all inquiries to:</h1>
+            <span>nsuroghon@gmail.com</span>
+          </h1>
+        </div>
+    </div>
   )
 }
 

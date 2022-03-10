be1982aab02b52e18cdd7e5bac52a8251730e5dc
niv suroghon <nivsuroghon@nivs-MacBook-Pro.local>
2022-03-10 08:59:01 -0500

    landing page front end updates

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import About from './pages/About';
 import Landing from './pages/Landing';
 import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path='/projects' element={<Projects />}/>
 
         <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+
       </Routes>
     </Router>
   );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineEdit } from 'react-icons/md'
 
 const Landing = () => {
   return (
@@ -7,17 +8,26 @@ const Landing = () => {
           <img className='landing__logo' alt='logo'></img>
         </div>
         <div className='landing__nav-box'>
+          <div className="btn-container">
+            <a className='btn btn--light'>Button</a>
+          </div>
+          <div className="btn-container">
+            <a className='btn btn--light'>Button</a>
+          </div>
+          <div className="btn-container">
+            <a className='btn btn--light'>Button</a>
+          </div>
+          {/* <a className='btn btn--light'>Button</a>
           <a className='btn btn--light'>Button</a>
-          <a className='btn btn--light'>Button</a>
-          <a className='btn btn--light'>Button</a>
+          <a className='btn btn--light'>Button</a> */}
         </div>
         <div className='landing__heading-box'>
-          <h1 className='heading-primary'></h1>
+          <h1 className='landing__heading-primary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna pharetra, tempus risus venenatis, commodo nibh. Morbi eros tellus, ultricies id ornare at, vehicula ut nisl. Donec eros augue, auctor nec justo maximus, placerat imperdiet lacus. Cras risus nisl, accumsan ac fringilla at, vulputate ut libero. Cras a sapien venenatis, interdum mi vel, laoreet libero.</h1>
         </div>
         <div className="landing__email-box">
-          <a href='#' className='btn btn--dark'>Email</a>
+          <a href='#' className='btn btn--dark'>Email <MdOutlineEdit style={{paddingTop: '0.3rem'}}/> </a>
         </div>
-        <div className='socialsBox'>
+        <div className='landing__socials-box'>
           <a href='#'>Social Media</a>
         </div>
     </div>

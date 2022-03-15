c222b2170f579b81a376bf9641f12c4e3033f962
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-03-15 11:08:48 -0400

    styling socials component

--- client/src/components/Socials.js
+++ client/src/components/Socials.js
@@ -1,32 +1,39 @@
 import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { FiGithub } from 'react-icons/fi'
+
 import { MdOutlineEmail } from 'react-icons/md'
 
 const Socials = () => {
   return (
-    <div className='socials'>
-        <ul>
-            <li>
-                <a> <FaGithub /> </a>
-            </li>
-            <li>
-                <a> <FaLinkedin /> </a>
-            </li>
-            <li>
-                <a> <MdOutlineEmail /> </a>
-            </li>
-        </ul>
-        {/* <a className='btn btn--light'>
-            <FaGithub />
-        </a>
-        <a className='btn btn--light'> 
-            <MdOutlineEmail /> 
-        </a>
-        <a className='btn btn--light'>
-            <FaLinkedin />
-        </a> */}
+    <div className="socials">
+        <div className="socials__icon-box">
+            <a className='socials__icon'>
+                <FiGithub />
+            </a>
+        </div>
+        <div className="socials__icon-box">
+            <a className='socials__icon'>
+                <FaLinkedinIn />
+            </a>
+        </div>
+        <div className="socials__icon-box">
+            <a className='socials__icon'>
+                <MdOutlineEmail />
+            </a>
+        </div>
+            {/* <ul className='socials__list'>
+                <li className='socials__item'>
+                    <a className='btn socials__btn--icon'> <FiGithub /> </a>
+                </li>
+                <li className='socials__item'>
+                    <a className='btn socials__btn--icon'> <FaLinkedinIn /> </a>
+                </li>
+                <li className='socials__item'>
+                    <a className='btn socials__btn--icon'> <MdOutlineEmail /> </a>
+                </li>
+            </ul>    */}
     </div>
-    
   )
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,35 +4,63 @@ import Socials from '../components/Socials'
 
 const Landing = () => {
   return (
-    <div className='landing'>
-        <div className='landing__logo-box'>
-          <img className='landing__logo' alt='logo'></img>
+
+    <section className="landing">
+        <div className="container">
+            <div className="content">
+                <div className="logo-box">
+                  <img className='logo' alt='logo'></img>
+                </div>
+                <div className="nav-box">
+                  <div className="btn-container">
+                    <a className='btn btn--landing btn--light'>Button</a>
+                  </div>
+                  <div className="btn-container">
+                    <a className='btn btn--landing btn--light'>Button</a>
+                  </div>
+                  <div className="btn-container">
+                    <a className='btn btn--landing btn--light'>Button</a>
+                  </div>
+                </div>
+                <div className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna pharetra, tempus risus venenatis, commodo nibh. Morbi eros tellus, ultricies id ornare at, vehicula ut nisl.
+                Donec eros augue, auctor nec justo maximus, placerat imperdiet lacus. Cras risus nisl, accumsan ac fringilla at, vulputate ut libero.
+                Cras a sapien venenatis, interdum mi vel, laoreet libero.
+                </div>
+                <div className="email-box">
+                  <a href='#' className='btn btn--landing btn--dark'>Email <MdOutlineEdit style={{paddingTop: '0.3rem'}}/> </a>
+                </div>
+          
+                <Socials />
+              
+                
+            </div>
         </div>
-        <div className='landing__nav-box'>
-          <div className="btn-container">
-            <a className='btn btn--light'>Button</a>
-          </div>
-          <div className="btn-container">
-            <a className='btn btn--light'>Button</a>
-          </div>
-          <div className="btn-container">
-            <a className='btn btn--light'>Button</a>
-          </div>
-          {/* <a className='btn btn--light'>Button</a>
-          <a className='btn btn--light'>Button</a>
-          <a className='btn btn--light'>Button</a> */}
-        </div>
-        <div className='landing__heading-box'>
-          <h1 className='landing__heading-primary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna pharetra, tempus risus venenatis, commodo nibh. Morbi eros tellus, ultricies id ornare at, vehicula ut nisl. Donec eros augue, auctor nec justo maximus, placerat imperdiet lacus. Cras risus nisl, accumsan ac fringilla at, vulputate ut libero. Cras a sapien venenatis, interdum mi vel, laoreet libero.</h1>
-        </div>
-        <div className="landing__email-box">
-          <a href='#' className='btn btn--dark'>Email <MdOutlineEdit style={{paddingTop: '0.3rem'}}/> </a>
-        </div>
-        <div className='landing__socials-box'>
-          {/* <a href='#'>Social Media</a> */}
-          <Socials />
-        </div>
-    </div>
+    </section>
+
+    // <div className='landing'>
+    //     <div className='landing__logo-box'>
+    //       <img className='landing__logo' alt='logo'></img>
+    //     </div>
+    //     <div className='landing__nav-box'>
+          // <div className="btn-container">
+          //   <a className='btn btn--landing btn--light'>Button</a>
+          // </div>
+          // <div className="btn-container">
+          //   <a className='btn btn--landing btn--light'>Button</a>
+          // </div>
+          // <div className="btn-container">
+          //   <a className='btn btn--landing btn--light'>Button</a>
+          // </div>
+    //     </div>
+    //     <div className='landing__heading-box'>
+    //       <h1 className='landing__heading-primary'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna pharetra, tempus risus venenatis, commodo nibh. Morbi eros tellus, ultricies id ornare at, vehicula ut nisl. Donec eros augue, auctor nec justo maximus, placerat imperdiet lacus. Cras risus nisl, accumsan ac fringilla at, vulputate ut libero. Cras a sapien venenatis, interdum mi vel, laoreet libero.</h1>
+    //     </div>
+    //     <div className="landing__email-box">
+    //       <a href='#' className='btn btn--landing btn--dark'>Email <MdOutlineEdit style={{paddingTop: '0.3rem'}}/> </a>
+    //     </div>
+    //     <Socials />
+    // </div>
   )
 }
 

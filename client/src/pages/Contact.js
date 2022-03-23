9377728cba9bd1c4965b977a419a6270eb9af747
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-03-23 18:49:18 -0400

    navigation added to contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,19 +1,24 @@
 import React from 'react'
 
 import { MdOutlineMarkEmailRead } from 'react-icons/md'
+import Navigation from '../components/Navigation'
 
 const Contact = () => {
   return (
     <div className="contact">
-      <div className='contact__icon-box'>
-        <MdOutlineMarkEmailRead size={200}/>
-      </div>
-      <div className="contact__inquiries">
+      <Navigation />
+      <div className='contact__content'>
+        <div className='contact__icon-box'>
+          <MdOutlineMarkEmailRead size={200}/>
+        </div>
+        <div className="contact__inquiries">
           <h1>
             <h1>Please send all inquiries to:</h1>
             <span>nsuroghon@gmail.com</span>
           </h1>
-        </div>
+       </div>
+
+      </div>
     </div>
   )
 }

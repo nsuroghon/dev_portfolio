93c68f71243b8e93eac24182644ee5578b394616
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-04-26 14:01:26 -0400

    styling grid and adding breakpoints for responsiveness

--- client/src/components/ProjectsGrid.js
+++ client/src/components/ProjectsGrid.js
@@ -5,22 +5,30 @@ const ProjectsGrid = () => {
 
   const {projects, displayProject, projectSelected} = useContext(ProjectsContext);
   return (
-    <div className="projects-grid">
+    <div className="grid">
 
         {projects.map((project) => 
-    
-          <div className='projects-grid__project-item'
-          key={project.id} 
-          onClick={() => displayProject(project)}
-          style={{backgroundImage: `url(${project.thumbnail})`}}
-          >
-            <div className="projects-grid__project-item-cover"
-                //  style={{backgroundImage: `url(${project.thumbnail})`}}
+          // <div className='grid__item'
+          //   key={project.id} 
+          //   onClick={() => displayProject(project)}
+          //   // style={{backgroundImage: `url(${project.thumbnail})`}}
+          //   >
+            <img className='grid__item'
+              src={project.thumbnail}
+              key={project.id} 
+              onClick={() => displayProject(project)}
+              // src="http://fakeimg.pl/360x360/111/?text=2"
+              alt={project.title}
+              height='360'
+              width='360'
             >
-              <div className="h2">{project.title}</div>
-              <p>{project.text}</p>
-            </div>
-          </div>
+
+            </img>
+            // {/* <div className="projects-grid__project-item-cover">
+            //   <div className="h2">{project.title}</div>
+            //   <p>{project.text}</p>
+            // </div> */}
+          // </div>
 
         )}
       </div>

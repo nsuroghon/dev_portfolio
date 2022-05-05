58c4349ce9258d36b7cae970e89b40b476238754
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-05-05 15:36:48 -0400

    updated project descriptions on the backend, and centering the descriptions on the U/I

--- client/src/components/SingleProject.js
+++ client/src/components/SingleProject.js
@@ -12,7 +12,7 @@ import ProjectsContext from '../context/ProjectsContext';
 
 const SingleProject = () => {
     const {projectSelected, removeProject} = useContext(ProjectsContext)
-    console.log(projectSelected.item.images);
+    console.log(projectSelected);
     // page movement
     // const scrollUp = () => {
     //   window.scrollBy({
@@ -30,17 +30,24 @@ const SingleProject = () => {
   return (
     <div className="featured">
       <div className="featured__content"> 
-        <div className="featured__metatitle">
+        {/* <div className="featured__metatitle">
           <h2>SpaceX</h2>
-        </div>
+        </div> */}
         <div className="featured__title">
-          <h1>SpaceX - crew dragon displays</h1>
+          {/* <h1>SpaceX - crew dragon displays</h1> */}
+          <h1>{projectSelected.item.title}</h1>
         </div>
-        <p className='featured__description'>Principal UI/UX Designer working on the Crew Displays Team for SpaceX. Working with an all-star team of software engineers, operations engineers, and NASA astronauts I led the UI/UX design for the final 3 years of the project. They also trusted me enough to contribute code like a software engineer !!!</p>
+        <div className='featured__description'>
+          {/* Principal UI/UX Designer working on the Crew Displays Team for SpaceX. 
+          Working with an all-star team of software engineers, operations engineers, 
+          and NASA astronauts I led the UI/UX design for the final 3 years of the project.
+         They also trusted me enough to contribute code like a software engineer !!! */}
+          <p>{projectSelected.item.text}</p>
+        </div>
         <div className='featured__more-info'>
-          <p style={{"color": 'white'}}>Role:</p><p>Developer</p>
-          <p style={{"color": 'white'}}>Agency:</p><p>SpaceX</p>
-          <p style={{"color": 'white'}}>Year:</p><p>2020</p>
+          <p style={{"color": 'white'}}>Role:</p><p>{projectSelected.item.role}</p>
+          <p style={{"color": 'white'}}>Agency:</p><p>{projectSelected.item.agency}</p>
+          <p style={{"color": 'white'}}>Year:</p><p>{projectSelected.item.year}</p>
         </div>
         <div className="featured__images">
 

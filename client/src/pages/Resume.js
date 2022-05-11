b8aed342ca238e9e65d4a8536c6f23752c66e123
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-05-11 15:24:42 -0400

    early styling of resume & skills

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -15,107 +15,70 @@ const Resume = () => {
   // }
 
   return isLoading ? <h3>Loading...</h3> : (
-    <div className='Wrapper'>
+    <div className='wrapper'>
       <Navigation />
 
       <div className="resume">
         <div className="resume__content">
           <div className="resume__education">
-            <h1>Education</h1>
-            {resumeData.education.map( (item) =>
-              <div className="education_item"
-                key={item.id}
-              >
-                <div className="metatitle">{item.degree}{item.major}</div>
-                <div className="insitution">{item.institution}</div>
-                <div className="date">{item.gradYear}</div>
-              </div>
-            )}
-          </div>
-
-          <div className="resume__skills">
-            <h1>Skills</h1>
-            {resumeData.skills.map( (item) =>
-              <div className="education_item"
+            <div className="resume__resume-header">
+              <h1>Education</h1>
+            </div>
+            <div className="information">
+              {resumeData.education.map( (item) =>
+                <div className="resume__education-item"
                 key={item.id}
                 >
-                <div className="metatitle">{item.type}</div>
-                
-                    {item.technology.map( (technology, index) => 
-                    <div key={index}>
-                      <ul>
-                        <li>{technology}</li>
-                      </ul>
-                    </div>
-                  )}
-                    {item.concepts.map( (concept, index) => 
-                    <div key={index}>
-                      <ul>
-                        <li>{concept}</li>
-                      </ul>
-                    </div>
-                  )}
-                <div className="insitution"></div>
-                <div className="date"></div>
+                  <h3>{item.institution} • {item.gradYear}</h3>
+                  <h2>{item.degree} - {item.major}</h2>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="resume__skills">
+
+              <div className="resume__resume-header">
+                <h1>Skills</h1>
               </div>
-            )}
+
+              <div className="resume__skills-information">
+
+                {resumeData.skills.map( (item) =>
+                <div className="resume__skills-item"
+                  key={item.id}
+                  >
+                  <h2 className="skill-name">{item.type}</h2>
+                    <div className='technology'>
+                      <h3>Technology</h3>
+                      <ul>
+                        {item.technology.map( (technology, index) => 
+                          <li key={index}>{technology}</li>
+                        )}
+                      </ul>
+                    </div>
+                    
+                    <div className='concepts'>
+                      <h3>Concepts</h3>
+                      <ul>
+                        {item.concepts.map( (concept, index) => 
+                          <li key={index}>{concept}</li>
+                        )}
+                      </ul>
+                    </div>
+                      
+                  <div className="insitution"></div>
+                  <div className="date"></div>
+                </div>
+                )}
+              </div>
           </div>
         </div>
       </div>
-  )
-
-      {/* <div className="skills__main-container">
-         <div className="skills__content-box">
-            <h1 className='skills__header'><span>Education</span></h1>
-            <div className="skills__data">
-              {
-                Data.education && Data.education.map((item)=>{
-                  return(
-                    <div>
-                       <div>
-                          <h3>{item.UniversityName}</h3>
-                          <p>
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-         </div> */}
-        {/* <div className="skills__content-box">
-          <h1 className='skills__header'><span>Skills</span></h1>     
-            <div className="skills__data">
-              {
-                Data.work && Data.work.map((item) => {
-                  return(
-                    <div className="">
-                       <div className="">
-                          <h3>{item.CompanyName}</h3>
-                          <p className="">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-
-                    </div>
-
-                  )
-                })
-              }
-            </div> 
-        </div> */}
 
         {/* DOWNLOAD RESUME BUTTON */}
-        {/* <div className='skills__email-box'>
+        <div className='skills__email-box'>
           <a className="btn btn--landing btn--dark" href="" target="_blank">Resume</a>
-        </div> */}
+        </div>
     </div>
   )
 }

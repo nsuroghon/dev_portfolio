af03dc4fb14ca3ee1b5d5bf73ed1c7fde21237bf
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-05-17 14:27:43 -0400

    responsive contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { MdOutlineMarkEmailRead } from 'react-icons/md'
 import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 
 const Contact = () => {
   return (
@@ -10,15 +11,16 @@ const Contact = () => {
       <div className='contact__content'>
         <div className='contact__icon-box'>
           <MdOutlineMarkEmailRead size={200}/>
+          <h1>Lets Work Together.</h1>
         </div>
         <div className="contact__inquiries">
-          <h1>
-            <h1>Please send all inquiries to:</h1>
-            <span className='contact__email'>nsuroghon@gmail.com</span>
-          </h1>
+            <h1>email me at:</h1>
+            <span className='contact__email'>
+              <a href='mailto:nsuroghon@gmail.com'>nsuroghon@gmail.com</a>
+            </span>
        </div>
-
       </div>
+      <Footer />
     </div>
   )
 }

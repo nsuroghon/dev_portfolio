126c46fc13cf190f86b3663d926ead2329c1cc29
niv suroghon <nivsuroghon@nivs-MBP.lan>
2022-05-19 16:01:53 -0400

    removing comments

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -8,44 +8,28 @@ import Footer from '../components/Footer';
 import ProjectsContext from '../context/ProjectsContext';
 
 const SingleProject = () => {
-    const {projectSelected, removeProject} = useContext(ProjectsContext)
+
+    const {projectSelected} = useContext(ProjectsContext)
     console.log(projectSelected);
-    // page movement
-    // const scrollUp = () => {
-    //   window.scrollBy({
-    //       top: -200,
-    //       behavior: 'smooth'
-    //   });
-    // };
-    // const scrollDown = () => {
-    //   window.scrollBy({
-    //       top: 200,
-    //       behavior: 'smooth'
-    //   });
-    // };
 
   return (
     <div className="featured">
       <div className="featured__content"> 
-        {/* <div className="featured__metatitle">
-          <h2>SpaceX</h2>
-        </div> */}
+       
         <div className="featured__title">
-          {/* <h1>SpaceX - crew dragon displays</h1> */}
           <h1>{projectSelected.item.title}</h1>
         </div>
+
         <div className='featured__description'>
-          {/* Principal UI/UX Designer working on the Crew Displays Team for SpaceX. 
-          Working with an all-star team of software engineers, operations engineers, 
-          and NASA astronauts I led the UI/UX design for the final 3 years of the project.
-         They also trusted me enough to contribute code like a software engineer !!! */}
           <p>{projectSelected.item.text}</p>
         </div>
+
         <div className='featured__more-info'>
           <p className='type'>Role:</p><p>{projectSelected.item.role}</p>
           <p className='type'>Agency:</p><p>{projectSelected.item.agency}</p>
           <p className='type'>Year:</p><p>{projectSelected.item.year}</p>
         </div>
+
         <div className="featured__images">
 
           {
@@ -55,38 +39,10 @@ const SingleProject = () => {
               )
             })
           }
-          {/* <img src='/assets/images/sample_1_new.jpg'></img>
-          <img src='/assets/images/Dev_Web_2.jpg'></img> */}
-
         </div>
-        {/* <div className="h1">{projectSelected.item.id}</div>
-        <div className="h1">{projectSelected.item.title}</div>
-        <div className="h1">{projectSelected.item.text}</div> */}
       </div>
       
       <Tools />
-
-      {/* <div className='tools'>
-        <div className="tool">
-          <a onClick={scrollUp}>
-            <span className='bar'></span>
-          </a>
-        </div>
-        <div className="tool">
-          <a href='#' onClick={removeProject}>
-            <span className='bar'></span>
-            <span className='bar'></span>
-            <span className='bar'></span>
-          </a>
-        </div>
-        <div className="tool">
-          <a onClick={scrollDown}>
-            <span className='bar'></span>
-          </a >
-        </div>
-      </div> */}
-
-      {/* <Footer /> */}
 
     </div>
   )
